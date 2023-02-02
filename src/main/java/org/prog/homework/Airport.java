73c3eed4c4f7eda6bc7610eb3ccb6ce731bf843d
oo965787 <oleksii.osypenko@telenor.se>
2023-02-02 20:07:34 +0100

    Session 04: Selenium Base

--- src/main/java/org/prog/homework/Airport.java
+++ src/main/java/org/prog/homework/Airport.java
@@ -2,18 +2,52 @@ package org.prog.homework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Airport {
 
+
     public final List<Plane> planes = new ArrayList<>();
 
+
+
     //if no plane found - throw exception
-    public Plane getPlaneByFlightIdAndDestination() {
+    public Plane getPlaneByFlightIdAndDestination() throws Exception{
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Введите номер нужного рейса");
+        String a= sc.nextLine();
+        System.out.println("Место назначения");
+        String b= sc.nextLine();
+        String getPlaneByFlightIdAndDestination = planes.stream()
+                .filter(plane -> plane.destination.equals(b))
+                .filter(plane -> plane.flightNumber.equals(a))
+                .map(plane -> plane.flightNumber)
+                .findAny().get();
+        System.out.println("Рейс найден  "+getPlaneByFlightIdAndDestination+"  ");
+        if (getPlaneByFlightIdAndDestination.isEmpty()){
+            throw new Exception();
+        }
         return null;
     }
 
     //if no plane found - throw exception
-    public List<Plane> planeByDestination() {
+    public List<Plane> planeByDestination()throws Exception {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Куда летим?");
+        String c= sc.nextLine();
+        List<String> planeByDestination = planes.stream()
+                .filter(plane -> plane.destination.equals(c))
+                .map(plane -> plane.flightNumber)
+                .collect(Collectors.toList());
+        System.out.println("Самолеты на Киев:");
+        for (String i : planeByDestination) {
+            System.out.println(i);
+        }
+        if (planeByDestination.isEmpty()){
+            throw new Exception();
+        }
+
         return null;
     }
 }
--- src/main/java/org/prog/homework/HomeworkMain.java
+++ src/main/java/org/prog/homework/HomeworkMain.java
@@ -1,8 +1,69 @@
 package org.prog.homework;
+import java.util.*;
+import java.util.stream.Collectors;
+import org.prog.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomeworkMain {
 
     public static void main(String... args) {
+        Airport airport= new Airport();
 
+
+
+        Plane P0 = new Plane();
+        P0.flightNumber = "DA3456";
+        P0.destination = "Dnipro";
+
+
+        Plane P1 = new Plane();
+        P1.flightNumber = "AA1122";
+        P1.destination = "Kiyv";
+
+        Plane P2 = new Plane();
+        P2.flightNumber = "AA1133";
+        P2.destination = "Kiyv";
+
+        Plane P3 = new Plane();
+        P3.flightNumber = "AA1451";
+        P3.destination = "Kiyv";
+
+        Plane P4 = new Plane();
+        P4.flightNumber = "AL3111";
+        P4.destination = "Lviv";
+
+        Plane P5 = new Plane();
+        P5.flightNumber = "AL1189";
+        P5.destination = "Lviv";
+
+        Plane P6 = new Plane();
+        P6.flightNumber = "AR8811";
+        P6.destination = "Rivne";
+
+        airport.planes.add(P0);
+        airport.planes.add(P1);
+        airport.planes.add(P2);
+        airport.planes.add(P3);
+        airport.planes.add(P4);
+        airport.planes.add(P5);
+        airport.planes.add(P6);
+
+        try {
+            airport.getPlaneByFlightIdAndDestination();
+        }
+        catch (Exception e){
+            System.out.println("Пусто");
+        }
+
+        try{
+            airport.planeByDestination();
+        }
+        catch (Exception e){
+            System.out.println("Пусто");
+        }
+
+        }
     }
-}
